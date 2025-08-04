be537dd06c13596925691ac2d055d437785cb472
Ricardo F Santos <88550038+ricardophg1@users.noreply.github.com>
2025-08-03 21:04:09 -0300

    feat: add ordoc air document components

---
+++ frontend/ordoc-ai-frontend/src/components/ordoc-air/documents/index.ts
@@ -0,0 +1,5 @@
+export { default as DocumentList } from './DocumentList';
+export { default as DocumentCard } from './DocumentCard';
+export { default as DocumentPreview } from './DocumentPreview';
+export { default as DocumentActions } from './DocumentActions';
+export { default as VersionHistory } from './VersionHistory';
